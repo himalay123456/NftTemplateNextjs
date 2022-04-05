ecd8015fbd8bc1e83710c2f3d6d0bb0b4d23c107
himalay123456 <himalay@solulab.com>
2022-04-05 16:11:17 +0530

    Metamask wallet integration done

---
+++ src/hooks/index.js
@@ -0,0 +1,4 @@
+import useEagerConnect from './useEagerConnectHook';
+import useInactiveListener from './useInactiveListenerHook';
+
+export { useEagerConnect, useInactiveListener }
---
+++ src/utils/connectors.js
@@ -0,0 +1,5 @@
+import { InjectedConnector } from '@web3-react/injected-connector'
+
+export const injected = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42, 132, 142, 111, 56, 97, 80001],
+})
