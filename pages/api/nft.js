111133a354f8175ca50d319255537bce19f41390
himalay123456 <himalay@solulab.com>
2022-04-05 18:38:18 +0530

    Winston logger added for backend

--- pages/api/nft.js
+++ pages/api/nft.js
@@ -1,8 +1,11 @@
 import { dbConnect } from 'src/utils/index';
 import Nft from 'src/models/nft';
+import logger from 'src/utils/logger';
+
 dbConnect();
 
 export default async function handler(req, res) {
+  logger.info('Inside show all nfts route')
   const data = await Nft.find({});
   res.status(200).json(data)
 }
---
+++ src/utils/logger.js
@@ -0,0 +1,57 @@
+import winston , { format } from 'winston';
+import moment from 'moment'
+import 'moment-timezone';
+
+// winston.emitErrs = true
+
+const appendTimestamp = format((info, opts) => {
+    if(opts.tz)
+      info.timestamp = moment().tz(opts.tz).format('MMMM Do YYYY, h:mm:ss a');
+    return info;
+  });
+  
+
+const logger = new winston.createLogger({
+    datePattern: 'DD-MM-YYYY-HH',
+    format: format.combine(
+        appendTimestamp({ tz: 'Asia/Kolkata' }),
+        // format.timestamp(),
+        format.json()
+    ),
+	transports: [
+		new winston.transports.File({
+			level: 'info',
+			filename: './logs/all-logs.log',
+			handleExceptions: true,
+			json: true,
+			maxsize: 5242880, // 5MB
+			maxFiles: 5, // if log file size is greater than 5MB, logfile2 is generated
+			colorize: true,
+            timestamp: true,
+		}),
+		new winston.transports.Console({
+			level: 'debug',
+			handleExceptions: true,
+			json: false,
+			colorize: true,
+			timestamp: true,
+		}),
+	],
+	exceptionHandlers: [
+		new winston.transports.File({
+			filename: './logs/exceptions.log',
+			timestamp: true,
+			maxsize: 5242880,
+			json: true,
+			colorize: true,
+		}),
+	],
+	exitOnError: false,
+})
+
+module.exports = logger
+module.exports.stream = {
+	write(message) {
+		logger.info(message)
+	},
+}
